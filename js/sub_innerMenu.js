dda171a1c71d3f8ee4cc9cc25267dfc38b8a76bf
Jalgaram <kgr424@naver.com>
2025-05-27 17:58:02 +0900

    2025-05-27

--- js/sub_innerMenu.js
+++ js/sub_innerMenu.js
@@ -1,6 +1,33 @@
-// 상세페이지
-$(function (){
-    $.get("./txt/sub.txt", function(data){
+$('.inner-menu ul li').eq(0).css({
+     'font-weight': '500',
+        'border-bottom': '2px solid black'
+});
+
+$('.inner-menu ul li').click(function () {
+    const index = $(this).index();
+
+    $('.inner-menu ul li').css({
+        'font-weight': '',
+        'border-bottom': ''
+    });
+
+    $(this).css({
+        'font-weight': '500',
+        'border-bottom': '2px solid black'
+    });
+
+    $('.inner-menu1, .inner-menu2, .inner-menu3, .inner-menu4').hide();
+    $('.inner-menu' + (index + 1)).show();
+});
+
+// 제품상세 등등등
+$(function () {
+    $.get("./txt/sub.txt", function (data) {
         $('.inner-menu1').html(data);
     })
+
+    $.get("./txt/sub2.txt", function (data) {
+        $('.inner-menu2').html(data);
+    })
+
 });
